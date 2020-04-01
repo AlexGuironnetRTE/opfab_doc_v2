cfbe0f9d26cae38f62e24426103c23736b979900
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-04-01 17:27:40 +0200

    Integrating into simple site

--- generate_html.sh
+++ generate_html.sh
@@ -2,26 +2,32 @@
 
 # Setting image dir
 
-repo_root=$(pwd)
-export imagesdir=$repo_root/"images"
-
 export source_folder=src
-export output_folder=html_output
+export output_folder=$(pwd)/website/documentation
+export image_folder=images
+export output_imagesdir=$output_folder/$imagesdir
 
 generate_html_from_index() {
     filepath=$1
     dirpath=$(dirname $1)
-    output_dirpath=$(echo $dirpath | sed "s/$source_folder/$output_folder/g" )
-    #filename=$(basename "./${filepath%.*}")
+    output_dirpath=$(echo $dirpath | sed "s $source_folder $output_folder g" )
+    imagesdir=$(realpath --relative-to=$output_dirpath $output_imagesdir)/$image_folder
     asciidoctor -a imagesdir=$imagesdir -D $output_dirpath $filepath
 };
 export -f generate_html_from_index;
 
 # Clear output_dir
-rm -r $repo_root/$output_folder/*
+rm -r $output_folder/*
 
 # Generate output
+## Images    echo imagesdir $imagesdir
+cp -r $source_folder/$image_folder $output_folder/$image_folder
+
+## Indexes for documents
 find $source_folder -path "*/index.adoc" -exec bash -c 'generate_html_from_index "$0"' {} \;
+
+## Root level documents
+find $source_folder -maxdepth 1 -path "*.adoc" -exec bash -c 'generate_html_from_index "$0"' {} \;
 
 
 
